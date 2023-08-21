0b78a739a71869f51c32f0bc6a2d808ee0e4a6e2
Zonbi <malefiz@gmail.com>
2023-08-21 16:07:36 +0200

    Add project files.

---
+++ MyMathProject/MyMath.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMathProject
+{
+    internal class MyMath
+    {
+        public static int Calc_ggT(int a, int b)
+        {
+            int z = 0;
+            while (b != 0)
+            {
+                z = a % b;
+                a = b;
+                b = z;
+            }
+            return a;
+        }
+
+        public static int Calc_kgV(int a, int b)
+        {
+            return (a * b) / Calc_ggT(a, b);
+        }
+
+        public static int clac_rekursive_ggT(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+
+            return clac_rekursive_ggT(b, a % b);
+        }
+
+
+        public static int calc_mean(int[] storage)
+        {
+            int sum = 0;
+            int m = 0;
+            foreach(int val in storage)
+            {
+                sum += val;
+                m++;
+            }
+
+            return sum / m;
+        }
+
+        
+        public static int calc_min(int[] storage)
+        {
+            int min = storage[0];
+            int next = 0;
+            foreach(int val in storage)
+            {
+                
+            }
+            
+        }
+
+    }
+}
---
+++ MyMathProject/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MyMathProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int parseInputString(string input)
+        {
+            if(int.TryParse(input, out double result))
+        }
+
+        public static int ReadInt()
+        {
+            string input;
+            do
+            {
+                Console.WriteLine("Gebe eine positive Zahl ein ");
+                input = Console.ReadLine();
+
+            } while ();
+        }
+    }
+}
