35f086b2faae43a2de73fec2437ec293f8662564
Zonbi <malefiz@gmail.com>
2023-12-03 17:26:08 +0100

    commit MergeAlgo 03.12.2023

---
+++ MergeSort/MergeSort.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgo
+{
+    internal class MergeSort
+    {
+        public void UseSortAlgo(int[] A, int left, int right)
+        {
+            if(left < right)
+            {
+               int mid = (left + right)/2;
+               UseSortAlgo(A,left, mid);
+               UseSortAlgo(A,mid+1, right);
+               Merge(A, left,mid, right);
+
+            }
+        }
+
+        public void Merge(int[] A, int left,int mid, int right)
+        {
+            int[] B = new int[right-left+1];
+
+            //zeiger
+            int i = left; 
+            int j = mid+1;
+            int k = 0;
+
+            while(i<= mid && j <= right)
+            {
+                if (A[i] < A[j])
+                {
+                    B[k] = A[i];
+                    i++; 
+                }
+                else
+                {
+                    B[k] = A[j];
+                    j++;  
+                }
+                k++;
+            }
+
+            while (i<= mid)
+            {
+                B[k] = A[i];
+                i++; 
+                k++;
+            }
+            while (j<= right)
+            {
+                B[k] = A[j];
+                j++; 
+                k++;
+            }
+
+            for(int m =left;m <= right; m++)
+            {
+                A[m] = B[m-left];
+            }
+            
+            
+        } 
+
+
+    }
+}
---
+++ MergeSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testarray = new int[] { 5, 8, 23, 6, 2, 6, 8, 132, 86 };
+            Console.WriteLine("InputArray: " + ArraytoString(testarray));
+            MergeSort mergeSort = new MergeSort();
+
+            mergeSort.UseSortAlgo(testarray, 0, testarray.Length-1);
+
+            Console.WriteLine("SortedArray: " + ArraytoString(testarray));
+        }
+
+        static string ArraytoString(int[] arr) 
+        {
+            string strarry = "[";
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i < arr.Length - 1)
+                {
+                    strarry += arr[i];
+                    strarry += ", ";
+                }
+                else
+                {
+                    strarry += arr[i];
+                }
+                
+            }
+            strarry += "]";
+            return strarry;
+        }
+    }
+}
